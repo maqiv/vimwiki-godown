ae847114df3eb45814bc9dec3d874eadf8fec3fc
s <sebastian.glinski@gmail.com>
2017-02-02 18:38:41 +0100

    temp state for transferring to desktop machine

--- main.go
+++ main.go
@@ -8,8 +8,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
+	"path/filepath"
+	"regexp"
 	"strconv"
+	"strings"
+
+	"github.com/russross/blackfriday"
 )
+
+const TRG_FILE_EXTENSION string = ".html"
+const RGX_MDWN_HYPERLINK string = `\[(?P<desc>.+)\]\((?P<link>(/?\w+)+(\.\w+))\)`
+
+var url_base_prefix string = "w"
 
 type Flags struct {
 	force            bool
@@ -28,8 +39,70 @@ func main() {
 
 	fl := parseArguments(os.Args)
 
-	c := ReadFile(fl.input_file)
-	fmt.Println(c)
+	// Check if file already exists and overwrite flag is not set
+	trg_file_path := build_trg_filepath(fl.input_file, fl.output_dir)
+	if _, err := os.Stat(trg_file_path); os.IsNotExist(err) && !fl.force {
+		fmt.Println("Conversion of file %v aborted: File does exist and force flag is set to 0.", trg_file_path)
+		os.Exit(0)
+	}
+
+	var md_in string
+	var md_in_raw []byte
+	var renderer blackfriday.Renderer
+	var doc_title string
+
+	// Set document title
+	doc_title = find_document_title()
+	renderer = blackfriday.HtmlRenderer(0, doc_title, fl.css_file)
+
+	// Read input file in markdown format
+	md_in = ReadFile(fl.input_file)
+	md_in_raw = []byte(md_in)
+
+	// Convert markdown content to html
+	html_out_raw := blackfriday.Markdown(md_in_raw, renderer, 0)
+	html_out := string(html_out_raw)
+
+	fmt.Println(string(html_out))
+}
+
+// TODO: complete function logic
+func find_document_title() string {
+	return "do title"
+}
+
+// Construct the target file path where the content will be saved later
+func build_trg_filepath(src_filepath string, trg_dir string) string {
+	var src_filename, file_basename, trg_filename string
+
+	src_filename = filepath.Base(src_filepath)
+	file_basename = strings.TrimSuffix(src_filename, filepath.Ext(src_filename))
+	trg_filename = file_basename + trg_file_extension
+
+	return filepath.Join(trg_dir, trg_filename)
+}
+
+// Add a prefix to relative links so that they work
+// with custom web server configurations
+func prefix_relative_hyperlink(mdwn_content string, hyperlink_mdwn_ident string, link_pref string, rel_link string) string {
+	var prefixed_rel_link string
+
+	// Find the solution here with regex stuff:
+	// https://play.golang.org/p/IeAJmtkwB7 (OLD)
+	// https://play.golang.org/p/NzQ3R8FHem (OLD)
+	// https://play.golang.org/p/c0DwYWV-gl
+
+	r := regexp.Compile(hyperlink_mdwn_ident)
+
+	r.ReplaceAllStringFunc(mdwn_content, func(sstr string) string {
+		var ret, formatted_url string
+
+		sm := r.FindSubmatch(sstr)
+		formatted_url = path.Join(link_pref, sm[2])
+		ret = fmt.Sprintf("[%v](%v)", sm[1], formatted_url)
+	})
+
+	return ret
 }
 
 // Commandline arguments are parsed like defined by vimwiki
